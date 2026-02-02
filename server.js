5a6a89cafd42487f433b13190d09e371acf12a78
tharun220603 <135967679+tharun220603@users.noreply.github.com>
2026-02-03 04:08:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: 'https://resipihub.netlify.app',
     credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
