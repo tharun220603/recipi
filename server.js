ab7ff1af615ff83a946bd5a620de898881bdce67
tharunprakash <tharunp2206@gmail.com>
2026-02-16 00:45:09 +0530

    Project push

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://resipihub.netlify.app",
+    origin: "https://resipihub.netlify.app",
     credentials: true,
   }),
 );
